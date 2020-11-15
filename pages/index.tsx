d5466828cecaec22f13bf65d00ffb959f0531e48
davidhernon <david.hernon1@gmail.com>
2020-11-15 17:57:48 +0100

    changed how the dynamic routing works to make it JAMstack friendly

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { NextPage } from 'next'
-
 import HomePage from '@src/containers/Home'
+import { useRouter } from 'next/dist/client/router'
 
-const Home: NextPage = () => <HomePage />
+const Home: NextPage = () => {
+  const router = useRouter()
+  const { iso } = router.query
+
+  const countryCode = typeof iso === 'string' ? iso.toUpperCase() : undefined
+
+  return <HomePage iso={countryCode} />
+}
 
 export default Home
--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -3,21 +3,21 @@ import Map from '@src/components/map'
 import { CountryDetails } from 'types/map-types'
 import { convertCountryDetailModuleToList } from '@src/utils/json-data-utils'
 
-const Home: React.FC = () => {
+const Home: React.FC<{ iso?: string }> = ({ iso = 'US' }) => {
   const { SITE_NAME } = process.env
   const { MAPBOX_TOKEN } = process.env // https://github.com/vercel/next.js/issues/6888
-  const countryCode = 'US' // harding coding this until we build ui to handle
+
   const [countryDetailsList, setCountryDetailsList] = React.useState<CountryDetails[]>([])
   React.useEffect(() => {
-    import(`../../../public/json/${countryCode}.json`).then((countryDetailModule) => {
+    import(`../../../public/json/${iso}.json`).then((countryDetailModule) => {
       const countryDetailsList = convertCountryDetailModuleToList(countryDetailModule).filter((cd) => cd !== undefined)
       setCountryDetailsList(countryDetailsList)
     })
-  }, [])
+  }, [iso])
   return (
     <>
       <h1 data-testid="helloH1" className="text-xl text-gray-900">
-        {SITE_NAME}, visa status for "{countryCode}"
+        {SITE_NAME}, visa status for "{iso}"
       </h1>
       {MAPBOX_TOKEN && <Map countryDetailsList={countryDetailsList} token={MAPBOX_TOKEN}></Map>}
     </>
