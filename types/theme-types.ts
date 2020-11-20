1939356c616cbf249a153c1fdf364021493c35ac
davidhernon <david.hernon1@gmail.com>
2020-11-20 09:55:06 +0100

    added improved TS theme typing

--- types/theme-types.ts
+++ types/theme-types.ts
@@ -1,14 +1,34 @@
+interface ColorSet {
+  50: string;
+  100: string;
+  200: string;
+  300: string;
+  400: string;
+  500: string;
+  600: string;
+  700: string;
+  800: string;
+  900: string;
+}
 export interface Theme {
-theme: {
+  theme: {
     extend: {
-    colors: {
-        'no-data': string,
-        'required': string,
-        'on-arrival': string,
-        'e-visa':  string,
-        'visa-free': string,
-        'covid-ban': string,
+      colors: {
+        'no-data': string;
+        required: string;
+        'on-arrival': string;
+        'e-visa': string;
+        'visa-free': string;
+        'covid-ban': string;
+      };
+      gray: ColorSet;
+      red: ColorSet;
+      amber: ColorSet;
+      emerald: ColorSet;
+      blue: ColorSet;
+      indigo: ColorSet;
+      purple: ColorSet;
+      pink: ColorSet;
     };
-    };
-};
+  };
 }
